71e1e6063904b8e1c6d24d0ad955cc4528e96053
权芹乐 <qlquan@zhcpa.cn>
2018-07-10 17:48:45 +0800

    init project structure as spring boot

---
+++ src/main/java/com/quanqinle/myworld/MyworldApplication.java
@@ -0,0 +1,12 @@
+package com.quanqinle.myworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyworldApplication.class, args);
+	}
+}
