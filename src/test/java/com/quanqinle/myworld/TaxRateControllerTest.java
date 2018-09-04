87740302f0a001415fce8c359c3e0666b1b8f049
权芹乐 <qlquan@zhcpa.cn>
2018-09-04 15:33:25 +0800

    add SpringBoot的测试，演示测controller

---
+++ src/test/java/com/quanqinle/myworld/TaxRateControllerTest.java
@@ -0,0 +1,65 @@
+package com.quanqinle.myworld;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.quanqinle.myworld.controller.TaxRateController;
+import com.quanqinle.myworld.dao.TaxRateRepository;
+import com.quanqinle.myworld.entity.po.TaxRate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional // 测试执行完毕恢复数据
+public class TaxRateControllerTest {
+
+	@Autowired
+	TaxRateRepository taxRateRepository;
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	String expectedJson;
+
+	@Before
+	public void setUp() throws JsonProcessingException {
+		Page<TaxRate> taxRatePage = taxRateRepository.findById(1, PageRequest.of(0, 10));
+		expectedJson = this.Obj2Json(taxRatePage);
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void testTaxRateController() throws Exception {
+		String uri = "/tax/income/15000";
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON)).andReturn();
+		int status = result.getResponse().getStatus();
+		String content = result.getResponse().getContentAsString();
+		Assert.assertEquals("错误，正确的返回值是200", 200, status);
+		Assert.assertEquals("错误，返回值与预期不符", expectedJson, content);
+	}
+
+	protected String Obj2Json(Object obj) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(obj);
+	}
+
+}
