41a138d76a8380bd7dd0bf362155cd475a0cc5b4
quanqinle <qlquan@zhcpa.cn>
2018-11-02 10:21:15 +0800

    enable @Async in main app class

--- src/main/java/com/quanqinle/myworld/MyWorldApplication.java
+++ src/main/java/com/quanqinle/myworld/MyWorldApplication.java
@@ -3,12 +3,14 @@ package com.quanqinle.myworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author quanqinle
  */
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 public class MyWorldApplication {
 
 	public static void main(String[] args) {
